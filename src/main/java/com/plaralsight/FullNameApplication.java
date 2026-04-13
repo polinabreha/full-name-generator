4cd186da18aced99eb23e499eab822d87b5529dd
polinabreha <polinabreha@gmail.com>
2026-04-13 15:16:04 -0400

    deleted some parts

--- src/main/java/com/plaralsight/FullNameApplication.java
+++ src/main/java/com/plaralsight/FullNameApplication.java
@@ -8,17 +8,17 @@ public class FullNameApplication {
         System.out.println("Please enter your name: ");
 
         System.out.print("First name:");
-        String firstName = scanner.nextLine().trim().toUpperCase();
+        String firstName = scanner.nextLine();
         firstName =  Character.toUpperCase(firstName.charAt(0)) +
                 firstName.substring(1).toLowerCase();
 
         System.out.print("Middle name:");
-        String middleName = scanner.nextLine().trim().toUpperCase();
+        String middleName = scanner.nextLine();
         middleName =  Character.toUpperCase(middleName.charAt(0)) +
                 middleName.substring(1).toLowerCase();
 
         System.out.print("Last name:");
-        String lastName = scanner.nextLine().trim().toUpperCase();
+        String lastName = scanner.nextLine();
         lastName =  Character.toUpperCase(lastName.charAt(0)) +
                 lastName.substring(1).toLowerCase();
 
